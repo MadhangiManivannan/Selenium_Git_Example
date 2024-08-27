c0070f1042f69443791df1eebf674e09b28422dd
MadhangiManivannan <madhangi.m2506@gmail.com>
2024-08-27 17:55:41 +0530

    fourth commit

--- basics/test_Example1.py
+++ basics/test_Example1.py
@@ -8,3 +8,12 @@ def test_Google():
     driver.implicitly_wait(30)
     assert driver.title == "Google"
     driver.close()
+
+
+def test_fb():
+    driver = webdriver.Chrome()
+    driver.get("http://www.fb.com")
+    driver.maximize_window()
+    driver.implicitly_wait(30)
+    assert driver.title == "fb"
+    driver.close()
